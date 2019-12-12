787b75d43e8dc47cff3824d358a32761e2587af3
qingyishi_destop <1697668797@qq.com>
2019-12-13 01:01:25 +0800

    1

--- 数据库/学生信息管理系统/sims.sql
+++ 数据库/学生信息管理系统/sims.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地MySQL
+ Source Server         : localhost_3306
  Source Server Type    : MySQL
  Source Server Version : 50562
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50562
  File Encoding         : 65001
 
- Date: 12/12/2019 22:59:32
+ Date: 13/12/2019 00:59:23
 */
 
 SET NAMES utf8mb4;
@@ -35,6 +35,8 @@ CREATE TABLE `course`  (
 -- ----------------------------
 INSERT INTO `course` VALUES ('5003001', 'JAVA进阶-javaweb', 4, '2019-10-10', '2019-12-13');
 INSERT INTO `course` VALUES ('5003002', '数据库系统概论', 5, '2019-09-20', '2019-11-15');
+INSERT INTO `course` VALUES ('5003003', '离散数学', 5, '2019-09-05', '2019-11-22');
+INSERT INTO `course` VALUES ('5003004', 'JAVA基础', 4, '2018-09-11', '2018-12-14');
 
 -- ----------------------------
 -- Table structure for stu_cours_sele
@@ -56,6 +58,8 @@ CREATE TABLE `stu_cours_sele`  (
 INSERT INTO `stu_cours_sele` VALUES ('201716040224', '5003001', 90);
 INSERT INTO `stu_cours_sele` VALUES ('201716040224', '5003002', 85);
 INSERT INTO `stu_cours_sele` VALUES ('201716040223', '5003002', 83);
+INSERT INTO `stu_cours_sele` VALUES ('201716040223', '5003003', 80);
+INSERT INTO `stu_cours_sele` VALUES ('201716040224', '5003003', 82);
 
 -- ----------------------------
 -- Table structure for student
@@ -82,13 +86,22 @@ INSERT INTO `student` VALUES ('201716040224', '刘文博', '男', '13253376824',
 -- ----------------------------
 DROP TABLE IF EXISTS `tea_cours_teach`;
 CREATE TABLE `tea_cours_teach`  (
+  `tct_id` int(11) NOT NULL AUTO_INCREMENT,
   `t_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `c_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`t_num`) USING BTREE,
-  INDEX `c_num`(`c_num`) USING BTREE,
+  `c_num` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`tct_id`) USING BTREE,
+  UNIQUE INDEX `c_num`(`c_num`) USING BTREE,
+  INDEX `t_num`(`t_num`) USING BTREE,
   CONSTRAINT `tea_cours_teach_ibfk_1` FOREIGN KEY (`t_num`) REFERENCES `teacher` (`t_num`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `tea_cours_teach_ibfk_2` FOREIGN KEY (`c_num`) REFERENCES `course` (`c_num`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of tea_cours_teach
+-- ----------------------------
+INSERT INTO `tea_cours_teach` VALUES (1, '10010610', '5003001');
+INSERT INTO `tea_cours_teach` VALUES (2, '10010611', '5003003');
+INSERT INTO `tea_cours_teach` VALUES (3, '10010610', '5003004');
 
 -- ----------------------------
 -- Table structure for teacher
@@ -102,6 +115,12 @@ CREATE TABLE `teacher`  (
   `t_birthday` date NOT NULL,
   PRIMARY KEY (`t_num`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
+INSERT INTO `teacher` VALUES ('10010610', '郑丽萍', '女', '讲师', '1979-07-13');
+INSERT INTO `teacher` VALUES ('10010611', '费选', '男', '主任', '1982-07-21');
 
 -- ----------------------------
 -- Table structure for user
